5a98ed8357a8501a57b25ded3866634b274c094e
AymanSdq <sedqiayman02@gmail.com>
2025-02-23 16:00:23 +0000

    Marketing Section Start

--- src/components/Marketing.tsx
+++ src/components/Marketing.tsx
@@ -7,8 +7,11 @@ const Marketing = () => {
                 <p className="text-sm text-center text-gray-400">The Nexcent blog is the best place to read about the latest membership insights, <br /> trends and more. See who's joining the community, <br /> read about how our community are increasing their membership income and lot's more.​</p>
                 <div className="w-full grid grid-cols-3 items-center gap-6 pt-6">
                     {/* Cards */}
-                    <div className="col-span-1 bg-red-700">
-                        
+                    <div className="col-span-1 h-[250px]  relative bg-red-700">
+                        c
+                        <div className="w-[80%] absolute bottom-0 bg-white border border-gray-900">
+                            cc
+                        </div>
                     </div>
                     <div className="col-span-1 bg-blue-700">
                         b
