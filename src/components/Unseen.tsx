5e35a5de0310126c098e46e2eb1d9198628c3d6d
AymanSdq <sedqiayman02@gmail.com>
2025-02-24 20:12:48 +0000

    Only Space

--- src/components/Unseen.tsx
+++ src/components/Unseen.tsx
@@ -10,7 +10,7 @@ const Unseen = () => {
             <div className="md:w-[60%] w-full flex flex-col  gap-6">
                 <h1 className=" font-semibold text-[36px] text-[#4D4D4D]">The unseen of spending three years at Pixelgrade</h1>
                 <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
-                <button className="text-white py-3 w-[130px] bg-[#4CAF4F] rounded-sm">Learn More</button>
+                <button className="text-white py-3 w-[130px] bg-[#4CAF4F] rounded-sm">Learn More</button> 
             </div>
         </section>
     )
